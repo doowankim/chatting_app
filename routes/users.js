d43e0a1fd237d395204a91902113ce03dfd6156b
Doowankim <rlaendhks11@gmail.com>
2020-04-02 12:19:56 +0900

    user Login

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const gravatar = require('gravatar');
+const jwt = require('jsonwebtoken');
 const userModel = require('../model/user');
 
 // @route POST localhost:3040/users/signup
@@ -38,6 +39,46 @@ router.post('/signup', (req, res) => {
                             .catch(err => res.json(err));
                     });
                 });
+            }
+        })
+        .catch(err => res.json(err));
+});
+
+// @route POST localhost:3040/users/login
+// @desc user login /return jwt
+// @access Public
+router.post('/login', (req, res) => {
+    userModel
+        .findOne({email: req.body.email})
+        .then(user => {
+            if(!user){
+                return res.json({
+                    msg: '사용자를 찾을 수 없습니다'
+                });
+            } else {
+                bcrypt
+                    .compare(req.body.password, user.password)
+                    .then(isMatch => {
+                        if(isMatch) {
+                            const payload = { id: user.id, name: user.name, avatar: user.avatar };
+                            jwt.sign(
+                                payload,
+                                process.env.SECRET,
+                                { expiresIn: 36000 },
+                                (err, token) => {
+                                    res.json({
+                                        success: true,
+                                        tokenInfo: 'Bearer ' + token
+                                    });
+                                }
+                            )
+                        } else {
+                            res.status(400).json({
+                                msg: '패스워드가 틀렸습니다'
+                            });
+                        }
+                    })
+                    .catch(err => res.json(err));
             }
         })
         .catch(err => res.json(err));
